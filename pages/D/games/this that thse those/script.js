de01a4efdd6de23c5e6ebc1afb57fb6332d86c41
Kauan Machado <machadob.kauan@gmail.com>
2025-08-12 18:32:48 -0300

    bug index.html resolvido

--- pages/D/games/this that thse those/script.js
+++ pages/D/games/this that thse those/script.js
@@ -1,6 +1,43 @@
-let obj = JSON.parse(questions)
 
-let question_hold = document.getElementById('question')
 
-question_hold.innerHTML = obj.questions[1].question_1;
+// fetch para receber as pergunts do json 
+
+fetch('quiz.json')
+    .then(response => response.json())
+    .then(data => {
+        let question_hold = document.getElementById('question');
+        question_hold.innerHTML = data.questions[0].question_1; 
+        resposta = data.questions[0].answer;
+        console.log(resposta)
+    })
+    .catch(error => console.error('Erro ao carregar JSON:', error));
+
+
+let opcao = document.getElementsByClassName('option-text')
+let options = document.getElementById('options')
+
+
+
+i = 0
+let array = []
+
+while (i < 4) {
+    const Textoptions = [ "This", "That", "These", "Those"]
+    randomIndex = Math.floor(Math.random() * Textoptions.length)
+    if (array.includes(randomIndex) == true) {
+
+    } else {
+        opcao.item(i).innerText = Textoptions[randomIndex]
+        array.push(randomIndex)
+        Textoptions.splice(randomIndex)
+        i += 1
+
+    }
+}
+
+// function Verificar() {
+//     if (resposta == )
+// }
+
+options.addEventListener('click', Verificar)
 
